7996aa289fa0a896521438c83e6affd5b058abc0
LycheeXixi <114654712+LycheeXixi@users.noreply.github.com>
2024-03-24 00:53:26 +0800

    Update style and start doing media queries

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.querySelector('.des-button').addEventListener('click', function() {
     window.open('Hongxi_CV.pdf', '_blank');
 });
+
+window.addEventListener('scroll', function() {
+    var aboutRight = document.querySelector('.about-right');
+    var aboutRightPosition = aboutRight.getBoundingClientRect().top;
+    console.log(aboutRightPosition)
+    var scrollPosition = window.scrollY + this.window.innerHeight;
+
+    if(scrollPosition > aboutRightPosition) {
+       aboutRight.classList.add("about-right-anim");
+    }
+});
