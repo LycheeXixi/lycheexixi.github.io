005fd25d10f3b160f62f2f39fce1ee0bf5b750fc
LycheeXixi <114654712+LycheeXixi@users.noreply.github.com>
2024-03-27 14:40:56 +0200

    add a video for signabc

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ document.querySelector('.des-button').addEventListener('click', function() {
 
 window.addEventListener('scroll', function() {
     var aboutRight = document.querySelector('.about-right');
-    var aboutRightPosition = aboutRight.getBoundingClientRect().top;
+    var aboutRightPosition = aboutRight.getBoundingClientRect().bottom;
     console.log(aboutRightPosition)
     var scrollPosition = window.scrollY + this.window.innerHeight;
+    console.log('s' + scrollPosition)
 
     if(scrollPosition > aboutRightPosition) {
        aboutRight.classList.add("about-right-anim");
+       console.log('show')
     }
 });
